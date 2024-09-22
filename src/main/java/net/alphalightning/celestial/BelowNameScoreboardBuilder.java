0039733b1535caa967346000e83649f17a7190ac
Merry <svgr1808@outlook.de>
2024-09-22 15:33:34 +0200

    Implement builder structure

---
+++ src/main/java/net/alphalightning/celestial/BelowNameScoreboardBuilder.java
@@ -0,0 +1,39 @@
+package net.alphalightning.celestial;
+
+import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collection;
+
+final class BelowNameScoreboardBuilder implements ScoreboardBase.Builder {
+
+    @Override
+    public ScoreboardBase.Builder type(@NotNull DisplayType type) {
+        return this;
+    }
+
+    @Override
+    public ScoreboardBase.Builder title(@NotNull Component title) {
+        return this;
+    }
+
+    @Override
+    public ScoreboardBase.Builder appendLine(@NotNull Component line) {
+        return this;
+    }
+
+    @Override
+    public ScoreboardBase.Builder appendEmptyLine() {
+        return this;
+    }
+
+    @Override
+    public ScoreboardBase.Builder appendLines(@NotNull Collection<Component> lines) {
+        return this;
+    }
+
+    @Override
+    public ScoreboardBase build() {
+        return null;
+    }
+}
