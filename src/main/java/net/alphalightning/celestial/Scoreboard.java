1e1dac24e1d9f71ed01072e6b9d5bc0633258ec2
Merry <svgr1808@outlook.de>
2024-09-22 16:38:00 +0200

    Add api for debugging scoreboards

--- src/main/java/net/alphalightning/celestial/Scoreboard.java
+++ src/main/java/net/alphalightning/celestial/Scoreboard.java
@@ -2,14 +2,20 @@ package net.alphalightning.celestial;
 
 import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface Scoreboard {
 
     void display();
 
     void destroy();
+
+    @NotNull Component title();
+
+    @UnmodifiableView @NotNull List<Component> lines();
 
     interface Builder<T> {
         T title(@NotNull Component title);
