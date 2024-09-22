1550776efcf752c2e5ed6544183a652c5a01c9bc
Merry <svgr1808@outlook.de>
2024-09-22 16:01:52 +0200

    Implement builder for below name scoreboards

--- src/main/java/net/alphalightning/celestial/BelowNameScoreboard.java
+++ src/main/java/net/alphalightning/celestial/BelowNameScoreboard.java
@@ -1,6 +1,18 @@
 package net.alphalightning.celestial;
 
+import net.kyori.adventure.text.Component;
+
+import java.util.LinkedList;
+
 public final class BelowNameScoreboard implements ScoreboardBase {
+
+    private final LinkedList<Component> lines;
+    private final Component title;
+
+    BelowNameScoreboard(Component title, LinkedList<Component> lines) {
+        this.title = title;
+        this.lines = lines;
+    }
 
     @Override
     public void display() {
