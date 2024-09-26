2b499f76bddc262beb0b25453149047e63a0c2f2
Merry <svgr1808@outlook.de>
2024-09-26 18:44:23 +0200

    Add lifecycles for scoreboards and teams

--- src/main/java/net/alphalightning/celestial/Lifecycle.java
+++ src/main/java/net/alphalightning/celestial/Lifecycle.java
@@ -6,4 +6,12 @@ public final class Lifecycle {
         CREATE, REMOVE, UPDATE
     }
 
+    public enum Scoreboard {
+        CHANGE, REMOVE
+    }
+
+    public enum Team {
+        CREATE, REMOVE, UPDATE, ADD_PLAYERS, REMOVE_PLAYERS
+    }
+
 }
--- src/main/java/net/alphalightning/celestial/SidebarScoreboard.java
+++ src/main/java/net/alphalightning/celestial/SidebarScoreboard.java
@@ -7,24 +7,43 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 final class SidebarScoreboard extends ReflectiveScoreboardBase implements ScoreboardBase {
 
     private final Component title;
     private final LinkedList<Component> lines;
+    private final String name;
 
     SidebarScoreboard(Player player, Component title, LinkedList<Component> lines) {
         super(player);
         this.title = title;
         this.lines = lines;
+        this.name = "celestial-" + Integer.toHexString(ThreadLocalRandom.current().nextInt());
     }
 
     @Override
     public void display() {
+        try {
+            sendObjectivePacket(Lifecycle.Objective.CREATE, name, title);
+            sendDisplayObjectivePacket(name);
+
+        } catch (Throwable throwable) {
+            throw new RuntimeException("Unable to create scoreboard", throwable);
+        }
     }
 
     @Override
     public void destroy() {
+        try {
+            for (var i = 0; i < lines.size(); i++) {
+                sendTeamPacket(name, i, Lifecycle.Team.REMOVE);
+            }
+            sendObjectivePacket(Lifecycle.Objective.REMOVE, name, title);
+
+        } catch (Throwable throwable) {
+            throw new RuntimeException("Unable to destroy scoreboard", throwable);
+        }
     }
 
     @Override
@@ -68,7 +87,7 @@ final class SidebarScoreboard extends ReflectiveScoreboardBase implements Scoreb
     }
 
     @Override
-    public void updateScores(Collection<Component> texts) {
+    public synchronized void updateScores(Collection<Component> texts) {
     }
 
     @Override
