a69bcfe5664125b63c60aecd9c2e8511da0a2069
Merry <svgr1808@outlook.de>
2024-09-26 18:53:36 +0200

    Add example plugin

---
+++ src/main/java/net/alphalightning/celestial/plugin/CelestialPlugin.java
@@ -0,0 +1,30 @@
+package net.alphalightning.celestial.plugin;
+
+import net.alphalightning.celestial.DisplayType;
+import net.alphalightning.celestial.Scoreboard;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CelestialPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        var miniMessage = MiniMessage.miniMessage();
+        var scoreboard = Scoreboard.builder(DisplayType.SIDEBAR)
+                .player(event.getPlayer())
+                .title(miniMessage.deserialize("<green>Example title"))
+                .build();
+
+        scoreboard.display();
+        Bukkit.getScheduler().runTaskLater(this, scoreboard::destroy, 40L);
+    }
+}
